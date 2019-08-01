44db95b4b6f7c1bbc9dee4bfbb37ced2deee0cc4
malostone <46139250+malostone@users.noreply.github.com>
2019-08-01 03:30:43 +0300

    Update 9.py

--- Lesson_1/9.py
+++ Lesson_1/9.py
@@ -1,3 +1,13 @@
 # 9.Вводятся три разных числа. Найти, какое из них
 # является средним (больше одного, но меньше другого).
 
+first = int(input("Введите первое число: "))
+second = int(input("Введите второе число: "))
+third = int(input("Введите третье число: "))
+
+if second < first < third or third < first < second:
+    print('Среднее число:', first)
+elif first < second < third or third < second < first:
+    print('Среднее число:', second)
+else:
+    print('Среднее число:', third)
