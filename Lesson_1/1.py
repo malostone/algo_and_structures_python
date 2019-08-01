e13e592e5015e0d74ee257f22f7496bce278b068
malostone <46139250+malostone@users.noreply.github.com>
2019-08-01 03:29:30 +0300

    Update 1.py

--- Lesson_1/1.py
+++ Lesson_1/1.py
@@ -1,1 +1,15 @@
 # 1.	Найти сумму и произведение цифр трехзначного числа, которое вводит пользователь.
+
+import math
+
+number = int(input("Введите трехзначное число :"))
+
+first = math.floor(number / 100)
+second = math.floor((number / 10) % 10)
+third = math.floor(number % 10)
+
+summ = first + second + third
+multiplication = first * second * third
+
+print(f"Сумма чисел: {summ} ")
+print(f"Произведение чисел: {multiplication}")
