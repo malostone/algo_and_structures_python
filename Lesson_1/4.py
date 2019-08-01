ceda2ead070c8cf177b03bc07a9990d90d8da980
malostone <46139250+malostone@users.noreply.github.com>
2019-08-01 03:30:03 +0300

    Update 4.py

--- Lesson_1/4.py
+++ Lesson_1/4.py
@@ -8,3 +8,40 @@
 то вводятся эти символы. Программа должна вывести на экран любой
 символ алфавита от 'a' до 'f' включительно.
 """
+
+import random,math
+
+min = int(input("Введите минимальную границу случайного числа: "))
+max = int(input("Введите максимальную границу случайного числа: "))
+
+def check_int(min,max): 
+    if min > max:
+        return "Минимальное число больше максимального"
+    else:
+        number = math.floor(random.random() * (max - min)) + min
+        return f"Случайное целое число {number}"
+    
+def check_float(min,max): 
+    if min > max:
+        return "Минимальное число больше максимального"
+    else:
+        number = random.random() * (max - min) + min
+        return f"Случайное вещественное  число {number}"
+    
+print(check_int(min,max))
+print(check_float(min,max))
+
+min_letter = input("Введите любой символ английского алфавита: ")
+max_letter = input("Введите любой символ английского алфавита: ")
+
+def check_letter(min,max):
+    max = ord(max)
+    min = ord(min)
+    if min > max:
+        return "Расположите буквы в алфавитном порядке"
+    else:
+        number = math.floor(random.random() * (max - min)) + min
+        symbol = chr(number)
+        return f"Случайный символ {symbol}"
+    
+print(check_letter(min_letter,max_letter))
