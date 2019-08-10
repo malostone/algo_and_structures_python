e07a896578074ae686465acc260193698b922ba4
malostone <46139250+malostone@users.noreply.github.com>
2019-08-10 19:11:35 +0300

    Update 3.py

--- Lesson_3/3.py
+++ Lesson_3/3.py
@@ -1,2 +1,39 @@
 #3.	В массиве случайных целых чисел поменять местами минимальный
 # и максимальный элементы.
+
+import random
+arr=[]
+for i in range(7):
+    arr.append(random.randint(2,99))
+    
+print(f'Изначальный массив:{arr}')
+
+first = arr[0]
+second = arr[1]
+if first < second:
+    maximum = second
+    minimum = first
+else:
+    maximum = first
+    minimum = second
+    
+for a in arr:
+    if a > maximum:
+        maximum = a
+    elif a < minimum:
+        minimum = a
+
+def getIndex(arr, num):
+    i=0
+    for el in arr:
+        if arr[i] == num:
+            return i
+        i+=1
+        
+a=getIndex(arr, maximum)
+b=getIndex(arr, minimum)
+
+arr[a] = minimum
+arr[b] = maximum
+
+print(f'Поменяли местами минимальное и максимальное значения:{arr}')
