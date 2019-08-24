db157858312bec6e2b336ae18e540ef989c0e5a9
malostone <46139250+malostone@users.noreply.github.com>
2019-08-25 01:57:08 +0300

    Update 3.py

--- Lesson_7/3.py
+++ Lesson_7/3.py
@@ -6,3 +6,25 @@
 массива. Но если это слишком сложно, то используйте метод сортировки,
  который не рассматривался на уроках
 """
+
+import random
+
+def getMedian(orig_list):
+    for i in range(len(orig_list)):
+        m = 0
+        n = 0
+        for j in range(len(orig_list)):
+            if orig_list[j] != orig_list[i]:
+                if orig_list[j] > orig_list[i]:
+                    n += 1
+                else:
+                    m += 1
+        if n == m:
+            return orig_list[i]
+
+randNumb = random.randint(1, 100)
+orig_list = [random.randint(1, 100) for _ in range(2*randNumb+1)]
+
+print(orig_list)
+a = getMedian(orig_list)
+print(a)
