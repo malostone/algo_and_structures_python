b48a038ee894efdf5f1b18b360e6c70f6316c5ba
malostone <46139250+malostone@users.noreply.github.com>
2019-08-25 01:55:59 +0300

    Update 1.py

--- Lesson_7/1.py
+++ Lesson_7/1.py
@@ -4,3 +4,27 @@
 исходный и отсортированный массивы. Сортировка должна быть реализована в
 виде функции. По возможности доработайте алгоритм (сделайте его умнее).
 """
+
+import random
+
+def bubble_sort(orig_list):
+    n = len(orig_list)-1
+    while n > 0:
+        i = len(orig_list)-1
+        swap = False
+        while i > len(orig_list)-n-1:
+            if orig_list[i] < orig_list[i-1]:
+                orig_list[i],orig_list[i-1] = orig_list[i-1],orig_list[i]
+                swap = True
+            i-=1
+        n -= 1
+        if swap == False:
+            break
+    return orig_list
+
+
+orig_list = [random.randint(-100, 100) for _ in range(100)]
+
+print(orig_list)
+a = bubble_sort(orig_list)
+print(a)
