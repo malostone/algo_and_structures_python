406c6453d41294422b4dc4e5d75468ea16e9edc7
malostone <46139250+malostone@users.noreply.github.com>
2019-08-29 00:55:17 +0300

    Update 2.py

--- Lesson_7/2.py
+++ Lesson_7/2.py
@@ -3,3 +3,43 @@
 заданный случайными числами на промежутке [0; 50). Выведите на экран исходный
 и отсортированный массивы.
 """
+
+import random
+
+arr = [random.randint(0, 50) for i in range(random.randint(5,15))]
+print(f"Изначальный массив:  {arr}")
+
+def merge_sort(arr):
+    if len(arr) > 1:
+        center = len(arr) // 2
+        left = arr[:center]
+        right = arr[center:]
+
+        merge_sort(left)
+        merge_sort(right)
+
+        i, j, k = 0, 0, 0
+
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                arr[k] = left[i]
+                i += 1
+            else:
+                arr[k] = right[j]
+                j += 1
+            k += 1
+
+        while i < len(left):
+            arr[k] = left[i]
+            i += 1
+            k += 1
+
+        while j < len(right):
+            arr[k] = right[j]
+            j += 1
+            k += 1
+        return arr
+    
+sortArr = merge_sort(arr) 
+
+print(f"Отсортированный массив:  {sortArr}")
